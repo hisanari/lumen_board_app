b68fb16722cbd74976b3738bd119564cb2c8aa18
hisanari <hisanari.1987@gmail.com>
2018-07-15 20:17:08 +0900

    add react Greeting

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Greeting from './Greeting';
 
 class App extends Component {
+  // 状態をもたせる
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'チャッピー',
+    };
+  }
+
+  handleMouseOver() {
+    this.setState({ name: 'Chaa!!!'});
+  }
+
+  handleMouseOut(){
+    this.setState({ name: 'チャッピー'});
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started!!, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div 
+        onMouseOver={() => this.handleMouseOver()}
+        onMouseOut={() => this.handleMouseOut()}
+      >  
+        <Greeting name={ this.state.name } />
       </div>
     );
   }
---
+++ src/frontend/src/Greeting.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Greeting(props) {
+    return (
+    <div> Hello, {props.name} </div>
+    );
+}
+
+export default Greeting;
